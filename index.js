bd2ef94a510a5b31526a93110801ba546d76d393
Mariglen Doda <mariglendoda1999@gmail.com>
2023-12-08 13:40:51 +0100

    Implementing a function and listener event when clicked the button

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('The game is running!');
+// Variables
+const startNewGameBtnElement = document.querySelector('.start-new-game');
+const board = document.querySelector('#game-board');
+
+function startGameFn() {
+  console.log('The game is clicked!');
+}
+
+startNewGameBtnElement.addEventListener('click', startGameFn)
